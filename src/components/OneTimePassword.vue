deeabe3d85f88d6f892d5fc58a66fb300320cb2e
gramblicka <daniel.gramblicka@ringier.sk>
2024-03-07 22:35:49 +0100

    changes & finish main component

--- src/components/OneTimePassword.vue
+++ src/components/OneTimePassword.vue
@@ -2,85 +2,96 @@
   <div class="otp-app">
     <div
         class="otp-wrapper"
-        :class="isInputFocused ? activeWrapperClassHandler() : wrapperClassHandler()"
-        :style="wrapperStyle()"
+        :class="[isInputFocused ? otpWrapperActive : classInput, customWrapperClass]"
+        :style="{direction: inputDirection, gap: inputsGapWidth, borderRadius}"
     >
       <input
-          v-for="(digitInputNumber, index) in digitsCount"
-          ref="digitInput"
+          :type="inputType === INPUT_PASSWORD ? 'password' : 'text'"
+          v-for="(digitInputNumber, index) in inputCount"
           :key="index + digitInputNumber"
-          v-model="inputValue[index]"
+          ref="digitInputCollection"
+          v-model="inputModel[index]"
           autocomplete="one-time-code"
-          class="otp-input"
-          :class="[inputClassHandler(), activeInput === index ? activeInputClassHandler() : '']"
+          class="otp-element-input"
+          :class="[
+              inputElementClass,
+              activeInput === index ? activeInputClass : '',
+              customInputClass
+              ]"
           :placeholder="placeholder"
           :disabled="isDisabled"
-          :style="inputStyle()"
-          @focus="onFocus(index)"
-          @blur="onBlur"
-          @paste="onPaste"
-          @input="onInput(index)"
-          @keydown="keydownHandler(index, $event)"
+          :style="{'borderRadius': otpMode === DIVIDED_MODE ? borderRadius : '0px'}"
+          @focus="onFocusTrigger(index)"
+          @blur="onBlurTrigger"
+          @paste="onPasteTrigger"
+          @input="onInputTrigger(index)"
+          @keydown="onKeydownTrigger(index, $event)"
       >
     </div>
-    <span
-        v-if="!isValid"
-        :class="errorClassHandler"
-    >
-      <slot name="errorMessage"/>
+    <span v-if="!isValid">
+      <slot/>
     </span>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {computed, defineProps, onMounted, defineEmits, ref, StyleValue} from "vue";
+import {defineProps, onMounted, defineEmits, ref} from "vue";
+
 const emit = defineEmits(['value', 'completed', 'changed', 'paste'])
+
 const INPUT_PASSWORD = 'password';
 const INPUT_TEXT = 'text';
 const INPUT_NUMBER = 'number';
 const allowedInputTypes = [INPUT_PASSWORD, INPUT_TEXT, INPUT_NUMBER];
 
+const GROUP_MODE = 'group';
+const DIVIDED_MODE = 'divided';
+const allowedModes = [GROUP_MODE, DIVIDED_MODE];
+
+const DIRECTION_LTR = 'ltr';
+//@ts-ignore
+const DIRECTION_RTL = 'rtl';
+
 const {
   borderRadius,
-  digitsCount,
-  mode,
+  inputCount,
+  otpMode,
   inputType,
   placeholder,
-  layoutGap,
+  inputsGapWidth,
   isDisabled,
   isValid,
-  autoFocus,
-  errorClass,
-  separateInputClass,
-  separateWrapperClass,
-  groupInputClass,
-  groupWrapperClass,
-  activeInputClass,
-  activeWrapperClass
+  isAutoFocus,
+  reverseDirection,
+  customWrapperClass,
+  customInputClass,
+  customActiveInputClass,
+  customWrapperValidClass,
+  customWrapperErrorClass,
 } = defineProps({
-  digitsCount: {
+  inputCount: {
     type: Number,
     default: 6,
   },
-  mode: {
+  otpMode: {
     type: String,
-    default: 'separate',
+    default: DIVIDED_MODE,
   },
   inputType: {
     type: String,
-    default: 'number',
+    default: INPUT_NUMBER,
   },
   placeholder: {
     type: String,
     default: '-',
   },
   borderRadius: {
-    type: Number,
-    default: 5,
+    type: String,
+    default: '5px',
   },
-  layoutGap: {
-    type: Number,
-    default: 10,
+  inputsGapWidth: {
+    type: String,
+    default: '10px',
   },
   isDisabled: {
     type: Boolean,
@@ -90,124 +101,70 @@ const {
     type: Boolean,
     default: true,
   },
-  autoFocus: {
+  isAutoFocus: {
     type: Boolean,
     default: true,
   },
-  errorClass: {
-    type: String,
-    default: '',
-  },
-  separateInputClass: {
-    type: String,
-    default: '',
-  },
-  separateWrapperClass: {
-    type: String,
-    default: '',
-  },
-  groupInputClass: {
-    type: String,
-    default: '',
-  },
-  groupWrapperClass: {
-    type: String,
-    default: '',
-  },
-  activeInputClass: {
-    type: String,
-    default: '',
-  },
-  activeWrapperClass: {
-    type: String,
-    default: '',
-  },
-  allowOnlyNumbers: {
+  reverseDirection: {
     type: Boolean,
-    default: true
+    default: false,
+  },
+  customWrapperClass: {
+    type: String,
+    default: '',
+  },
+  customInputClass: {
+    type: String,
+    default: '',
+  },
+  customActiveInputClass: {
+    type: String,
+    default: 'otp-input-active',
+  },
+  customWrapperValidClass: {
+    type: String,
+    default: 'otp-wrapper-active',
+  },
+  customWrapperErrorClass: {
+    type: String,
+    default: 'otp-wrapper-error',
   }
 });
 
-const inputValue = ref([]);
+const inputModel = ref<Array<string>>([]);
 const joinedValue = ref('');
 const isInputFocused = ref(false);
 const activeInput = ref(-1);
-const digitInput = ref(null);
+const digitInputCollection = ref<Array<HTMLInputElement>>([]);
+const BACKSPACE_KEY = 'Backspace';
+const inputDirection = reverseDirection ? DIRECTION_RTL : DIRECTION_LTR;
 
-const wrapperStyle = (): StyleValue => {
-  return {
-    direction: 'ltr',
-    gap: `${layoutGap}px`,
-    'border-radius': `${borderRadius}px`,
-  };
-}
-
-const inputStyle = (): {"--border-radius": string} => {
-  return {
-    '--border-radius': `${borderRadius}px`
-  }
-}
-
-const inputClassHandler = (): any => {
-  if (mode === 'separate') {
-    if (isValid) {
-      return separateInputClass ? separateInputClass : 'default-input-separate';
-    } else {
-      return separateInputClass ? separateInputClass : 'default-error-input-separate';
-    }
-  }
-  if (mode === 'group') {
-    return groupInputClass ? groupInputClass : 'default-input-group';
-  }
-}
-
-const activeInputClassHandler = (): string => {
-  if (mode === 'separate') {
-    return activeInputClass ? activeInputClass : 'default-active-input';
-  }
-  return '';
-}
-
-const activeWrapperClassHandler = (): string => {
-  if (mode === 'group') {
-    if (isValid) {
-      return activeWrapperClass ? activeWrapperClass : 'default-active-wrapper';
-    } else {
-      return activeWrapperClass ? activeWrapperClass : 'default-error-wrapper-group';
-    }
-  }
-  return '';
-}
-
-const wrapperClassHandler = (): string => {
-  if (mode === 'separate') {
-    return separateWrapperClass ? separateWrapperClass : 'default-wrapper-separate';
-  }
-  if (mode === 'group') {
-    if (isValid) {
-      return groupWrapperClass ? groupWrapperClass : 'default-wrapper-group';
-    } else {
-      return groupWrapperClass ? groupWrapperClass : 'default-error-wrapper-group';
-    }
-  }
-  return '';
-}
-
-const errorClassHandler = computed((): string => {
-  return errorClass ? errorClass : 'default-error-class';
-})
+const classInput = otpMode === GROUP_MODE ? 'otp-input-group' : 'otp-element-input-divided';
+const otpWrapperValidClass = isValid ? customWrapperValidClass : customWrapperErrorClass;
+console.log('otpis');
+console.log(otpWrapperValidClass);
+const otpWrapperActive = otpMode === GROUP_MODE ? otpWrapperValidClass : '';
+console.log('resu');
+console.log(otpWrapperActive);
+const dividedModeValid = isValid ? 'otp-element-input-divided' : 'otp-wrapper-divided-err';
+const inputElementClass = otpMode === DIVIDED_MODE ? dividedModeValid : 'otp-input-group'
+const activeInputClass = otpMode === DIVIDED_MODE ? customActiveInputClass : '';
 
 
-onMounted(() => {
-  if (autoFocus && !isDisabled) {
-    onFocus(0);
-    // @ts-ignore
-    digitInput.value[0].focus();
+/**
+ * Handle initialization, set focus on first input, set input types and handle whitelisted characters on user input
+ * @return {void}
+ */
+const init = (): void => {
+  onFocusTrigger(0);
+  if (digitInputCollection.value[0]) {
+    const currentDigitInput: HTMLInputElement = digitInputCollection.value[0];
+    currentDigitInput.focus();
     if (allowedInputTypes.indexOf(inputType) !== -1) {
-      // @ts-ignore
-      digitInput.value.forEach((input: any) => {
+      digitInputCollection.value.forEach((input: any) => {
         input.type = inputType;
 
+        //in case of number mode - replace all characters except digits
         if (inputType === 'number') {
           input.addEventListener('input', () => {
             input.value = input.value.replace(/\D/g, '');
@@ -215,84 +172,137 @@ onMounted(() => {
         }
       });
     }
+  } else {
+    console.warn('ref digit input is empty')
   }
-})
+}
+
+/**
+ * Init check otp mode
+ * @return {void}
+ */
+const checkOtpMode = (): void => {
+  if (!allowedModes.includes(otpMode)) {
+    console.warn(`Prop otpMode: "${otpMode}" is not found. Please choose from existing modes: ${allowedModes.join(',')}`)
+  }
+}
 
 // @ts-ignore
 const reset = (): void => {
-  inputValue.value = [];
+  inputModel.value = [];
   joinedValue.value = '';
 }
 
-const BACKSPACE_KEY_NUMBER = 8;
-const keydownHandler = (index: number, e: any): void => {
-  if (e.keyCode === BACKSPACE_KEY_NUMBER && e.target.value === '') {
+/**
+ * Handle backspace in case of divided inputs
+ * @param index {number}
+ * @param event {KeyboardEvent}
+ * @return {void}
+ */
+const onKeydownTrigger = (index: number, event: KeyboardEvent): void => {
+  const target = event.target as HTMLInputElement;
+  if (event.key === BACKSPACE_KEY && target.value === '') {
     // @ts-ignore
-    digitInput.value[Math.max(0, index - 1)].focus();
+    digitInputCollection.value[Math.max(0, index - 1)].focus();
   }
 }
 
-const onInput = (index: number): void => {
-  const [first, ...rest] = inputType === INPUT_NUMBER
-      // @ts-ignore
-      ? inputValue.value[index].replace(/[^0-9]/g, '')
-      : inputValue.value[index];
-  // @ts-ignore
-  inputValue.value[index] = first === null || first === undefined ? '' : first;
-  const lastInputBox = index === digitsCount - 1;
-  const insertedContent = first !== undefined;
-  if (insertedContent && !lastInputBox) {
-    // @ts-ignore
-    digitInput.value[index + 1].focus();
-    // @ts-ignore
-    digitInput.value[index + 1].value = rest.join('');
-    // @ts-ignore
-    digitInput.value[index + 1].dispatchEvent(new Event('input'));
+/**
+ * Handle keydown (input) changes
+ * Fill current active input, focus next and dispatch event and emit change/completed of changed input value
+ * @param index {number}
+ * @return {void}
+ */
+const onInputTrigger = (index: number): void => {
+  if (!inputModel.value || !inputModel.value[index]) {
+    return
   }
-  joinedValue.value = inputValue.value.map((value) => value).join('');
+  const inputModelValue: string[] = inputModel.value;
+  const [first, ...rest] = inputType === INPUT_NUMBER
+      ? inputModelValue[index].replace(/[^0-9]/g, '')
+      : inputModelValue[index];
+  inputModelValue[index] = !first ? '' : first;
+  const lastInputBox = index === inputCount - 1;
+
+  if (first && !lastInputBox && digitInputCollection.value) {
+    const digitInputValue = digitInputCollection.value;
+    digitInputValue[index + 1].focus();
+    digitInputValue[index + 1].value = rest.join('');
+    digitInputValue[index + 1].dispatchEvent(new Event('input'));
+  }
+  joinedValue.value = inputModel.value.map((value) => value).join('');
   emit('value', joinedValue.value);
-  if (joinedValue.value.length === digitsCount) {
+  if (joinedValue.value.length === inputCount) {
     onComplete(joinedValue.value);
   }
-  onChanged();
+  onChangedEmit();
 }
 
-const onFocus = (index: number): void => {
+/**
+ * Activate input on focus
+ * @param index
+ * @return {void}
+ */
+const onFocusTrigger = (index: number): void => {
   activeInput.value = index;
   isInputFocused.value = true;
 }
 
-const onBlur = (): void => {
+/**
+ * Decrease active input to previous and disable focused input
+ * @return {void}
+ */
+const onBlurTrigger = (): void => {
   activeInput.value = -1;
   isInputFocused.value = false;
 }
 
+/**
+ * If all inputs are filled, blur fields and emit completed event
+ * @param value
+ * @return {void}
+ */
 const onComplete = (value: any): void => {
-  onBlur();
-  const number = digitsCount - 1;
-  if (digitInput.value && digitInput.value[number]) {
-    //@ts-ignore
-    digitInput.value[number].blur();
+  onBlurTrigger();
+  const number = inputCount - 1;
+  if (digitInputCollection.value && digitInputCollection.value[number]) {
+    digitInputCollection.value[number].blur();
   }
-  if (inputType === INPUT_PASSWORD) {
-    emit('completed', value);
-  } else {
-    emit('completed', value)
-  }
+  emit('completed', value)
 }
 
-const onChanged = (): void => {
-  emit('changed', inputValue.value.map((value) => value).join(''));
+/**
+ * Emit onchange event triggered by input value
+ * @return {void}
+ */
+const onChangedEmit = (): void => {
+  emit('changed', inputModel.value.map((value) => value).join(''));
 }
 
-const onPaste = (event: any): void => {
+/**
+ * Emits clipboard event on paste
+ * @param event ClipboardEvent
+ * @return {void}
+ */
+const onPasteTrigger = (event: ClipboardEvent): void => {
   emit('paste', event);
 }
+
+/**
+ * Mounted hook for app initialization
+ */
+onMounted(() => {
+  console.log(inputType);
+  if (isAutoFocus && !isDisabled) {
+    checkOtpMode()
+    init()
+  }
+})
 </script>
 
 <style>
 div.otp-app {
-  width: max-content;
+  width: 100%;
 }
 
 div.otp-app div.otp-wrapper {
@@ -304,21 +314,34 @@ div.otp-app div.otp-wrapper {
   transition-property: all;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   transition-duration: 0.3s;
+  overflow: hidden;
 }
 
-div.otp-app div.otp-wrapper input.otp-input {
+div.otp-app div.otp-wrapper.otp-wrapper-error {
+  border: 1px solid #af0000;
+}
+
+div.otp-app div.otp-wrapper input.otp-element-input {
   transition-property: all;
   transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
   transition-duration: 0.3s;
   border-radius: 5px;
 }
 
-div.otp-app div.otp-wrapper input.otp-input:disabled {
+div.otp-app div.otp-wrapper input.otp-element-input:disabled {
   background-color: #ececec !important;
 }
 
-div.otp-app div.otp-wrapper input.default-input-separate,
-div.otp-app div.otp-wrapper input.default-error-input-separate {
+div.otp-app div.otp-wrapper input.otp-element-input-divided {
+  text-align: center;
+  font-weight: 300;
+  background-color: transparent;
+  border: solid 1px #dedede;
+  width: 40px;
+  height: 40px;
+}
+
+div.otp-app div.otp-wrapper input.otp-wrapper-divided-err {
   text-align: center;
   font-weight: 300;
   background-color: transparent;
@@ -327,19 +350,11 @@ div.otp-app div.otp-wrapper input.default-error-input-separate {
   height: 40px;
 }
 
-div.otp-app div.default-wrapper-separate {
-  background: transparent;
+div.otp-app div.otp-input-group {
+  border: solid 1px #dedede;
 }
 
-div.otp-app div.default-wrapper-group {
-  border: solid 2px #ececec;
-}
-
-div.otp-app div.default-error-wrapper-group {
-  border: solid 2px #d50000;
-}
-
-div.otp-app input.default-input-group {
+div.otp-app input.otp-input-group {
   background-color: transparent;
   font-weight: 300;
   border: none;
@@ -350,23 +365,19 @@ div.otp-app input.default-input-group {
 
 
 @media only screen and (max-width: 600px) {
-  div.otp-app input.default-input-group {
+  div.otp-app input.otp-input-group {
     width: 2.5rem;
     height: 40px;
   }
 }
 
-div.otp-app input.default-active-input {
-  border: solid 2px #525252;
+/*div.otp-app div.otp-wrapper input.otp-input-divided*/
+div.otp-app div.otp-wrapper input.otp-input-active {
+  border: solid 1px #525252;
 }
 
-div.otp-app div.default-active-wrapper {
-  border: solid 2px #525252;
-}
-
-div.otp-app span.default-error-class {
-  color: #d50000;
-  font-weight: 300;
+div.otp-app div.otp-wrapper-active {
+  border: solid 1px #525252;
 }
 
 input:focus {
@@ -383,5 +394,4 @@ input::-webkit-inner-spin-button {
 input[type=number] {
   -moz-appearance: textfield;
 }
-
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import { createApp } from 'vue'
-// import './style.css'
 import OneTimePassword from './components/OneTimePassword.vue'
 
 createApp(OneTimePassword).mount('#app')
